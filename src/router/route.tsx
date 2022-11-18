5e76cf99b6008953d19c97898fb40492fd3269d0
mobina7849 <mobina.abbasi7849@gmail.com>
2022-11-18 16:24:31 +0330

    fixed file name

--- src/router/route.tsx
+++ src/router/route.tsx
@@ -1,5 +1,5 @@
 import {createBrowserRouter} from "react-router-dom";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import AddMoney from "../pages/addMoney";
 import Charts from "../pages/Charts";
 import Layout from "../Components/layout/Layout";
